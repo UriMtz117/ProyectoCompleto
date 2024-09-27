135e8bf59547393387f11952abd8ebf4e1410433
UriMtz117 <urielrm.ti22@utsjr.edu.mx>
2024-09-27 00:39:06 -0600

    first commit

--- backend/dist/index.dev.js
+++ backend/dist/index.dev.js
@@ -7,6 +7,8 @@ var cors = require('cors');
 var rutasUsuarios = require('./rutas/rutasUsuarios'); // Importa las rutas de usuarios
 
 
+require("dotenv").config();
+
 var app = express();
 app.use(cors());
 app.use(express.json()); // Habilitamos la recepción de JSON
@@ -14,6 +16,7 @@ app.use(express.json()); // Habilitamos la recepción de JSON
 
 app.use('/usuarios', rutasUsuarios); // Asegúrate de que esta línea esté incluida
 
+var port = process.env.PORT || 3000;
 var PORT = process.env.PORT || 3000;
 app.listen(PORT, function () {
   console.log("Servidor en funcionamiento en puerto ".concat(PORT));
