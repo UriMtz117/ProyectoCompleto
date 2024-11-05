2484f821c5b0999be2a28a91d35772044fb33c5d
UriMtz117 <urielrm.ti22@utsjr.edu.mx>
2024-11-05 09:08:09 -0600

    Update index.js

--- index.js
+++ index.js
@@ -1,16 +1,36 @@
 const express = require('express');
 const cors = require('cors');
-const rutasUsuarios = require('./rutas/rutasUsuarios');  // Importa las rutas de usuarios
-require ("dotenv").config();
+const rutasUsuarios = require('./rutas/rutasUsuarios');
+const rutasProductos = require('./rutas/rutasProductos');
+const rutasVentas = require('./rutas/rutasVentas');
+require('dotenv').config();
+const admin = require('firebase-admin');
+const serviceAccount = require('./keys.json');
+
+if (!admin.apps.length) {
+  admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount),
+    databaseURL: "https://miejemplo-3b20f.firebaseio.com"
+  });
+}
+
+const db = admin.firestore();
+
 const app = express();
-
 app.use(cors());
-app.use(express.json());  // Habilitamos la recepción de JSON
+app.use(express.json());
 
-// Usar las rutas para usuarios
-app.use('/usuarios', rutasUsuarios);  // Asegúrate de que esta línea esté incluida
-var port=process.env.PORT || 3000;
+// Middleware para pasar Firestore a las rutas
+app.use((req, res, next) => {
+  req.db = db;
+  next();
+});
+
+// Usar las rutas para usuarios, productos y ventas
+app.use('/usuarios', rutasUsuarios);
+app.use('/productos', rutasProductos);
+
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-    console.log(`Servidor en funcionamiento en puerto ${PORT}`);
+  console.log(`Servidor en funcionamiento en puerto ${PORT}`);
 });
